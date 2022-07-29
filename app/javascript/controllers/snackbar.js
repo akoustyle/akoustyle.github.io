2272b51b4248ead1216897e43f7c80539bf3f368
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-07-29 05:38:21 +0200

    adding a new home page

--- app/javascript/controllers/snackbar.js
+++ app/javascript/controllers/snackbar.js
@@ -1,29 +1,29 @@
-import { Controller } from "stimulus"
+// import { Controller } from "stimulus"
 
-export default class extends Controller {
+// export default class extends Controller {
 
-  static targets = ["snackbar"]
-  static timeout
+//   static targets = ["snackbar"]
+//   static timeout
 
-  connect() {
-    console.log("Snackbar controller is connected")
-  }
+//   connect() {
+//     console.log("Snackbar controller is connected")
+//   }
 
-  show(){
-    // remove previous snackbar
-    this.snackbarTarget.classList.remove('show')
-    clearTimeout(this.timeout)
-    setTimeout(()=>{
-      this.snackbarTarget.classList.add("show");
+//   show(){
+//     // remove previous snackbar
+//     this.snackbarTarget.classList.remove('show')
+//     clearTimeout(this.timeout)
+//     setTimeout(()=>{
+//       this.snackbarTarget.classList.add("show");
 
-      // After 2.9s(when it's faded out), remove show class
-      this.timeout = setTimeout(() => {
-        console.log('timing out')
-        this.snackbarTarget.classList.remove('show')
-      }, 2900);
-    },10)
+//       // After 2.9s(when it's faded out), remove show class
+//       this.timeout = setTimeout(() => {
+//         console.log('timing out')
+//         this.snackbarTarget.classList.remove('show')
+//       }, 2900);
+//     },10)
 
 
-  }
+//   }
 
-}
+// }
