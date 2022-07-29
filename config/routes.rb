af0db33bd080a343681387d449253df7dbdc8f64
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-07-29 07:03:59 +0200

    adding to routes new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :projects, param: :slug
-  root to: 'pages#home'
+  root to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "projects", to: "projects#index"
   get "new_project", to: "projects#new"
-  get "index", to: "pages#index"
+  get "old_page", to: "pages#home"
 end
